101901f5dc3c4e176a8782304cca6eaf00b37b54
element3309 <element3309@gmail.com>
2023-12-27 17:12:28 +0300

    some optimizations

--- Core/Src/platform/src/drv/plt_uart.c
+++ Core/Src/platform/src/drv/plt_uart.c
@@ -1,3 +1,6 @@
 #include "plt_uart.h"
 
+__attribute__((weak)) void plt_uart_transmit(plt_uart_t *uart, uint8_t* data, uint16_t size)
+{
 
+}
--- Core/Src/platform/src/plt_scheduler.c
+++ Core/Src/platform/src/plt_scheduler.c
@@ -1,6 +1,6 @@
 #include "plt_scheduler.h"
-
 #include "drv/plt_uart.h"
+#include <stdio.h>
 
 uint8_t some_data[12] = "Hello world!";
 
@@ -8,8 +8,9 @@ plt_uart_t plt_uart = {
 		3
 };
 
+
 void plt_scheduler(void)
 {
-	plt_uart_transmit(&plt_uart, some_data, 12);
+	plt_uart_transmit(&plt_uart, some_data, sizeof(some_data));
 }
 
